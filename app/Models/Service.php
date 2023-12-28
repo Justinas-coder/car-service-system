c2f0fc3e9de24cdcd258362ba15ec1f8f9325306
Justinas <justinas@Justinass-Air.lan>
2023-12-28 23:33:25 +0200

    relaishions

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -1,22 +1,22 @@
 <?php
-
 namespace App\Models;
-
 use App\Casts\Price;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class Service extends Model
 {
     use HasFactory;
-
     protected $casts = [
         'price' => Price::class
     ];
-
     protected $fillable = [
         'name',
         'description',
         'price'
     ];
+    public function invoice(): BelongsTo
+    {
+        return $this->belongsTo(Invoice::class);
+    }
 }
